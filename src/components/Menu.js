54ae6e233a34b86a4c73dfccb674f0afc1ad9ca7
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-13 15:31:37 -0300

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -52,7 +52,7 @@ const Menu = ({ isMenuOpen, setIsMenuOpen, addToCart, cart }) => {
                   src={product.image}
                   alt={product.name}
                   className="w-full h-40 object-cover rounded-lg mb-2"
-                  onError={(e) => (e.target.style.display = 'none')} // Esconde a imagem se houver erro
+                  onError={(e) => (e.target.style.display = 'none')}
                 />
               ) : (
                 <div className="w-full h-40 bg-gray-200 rounded-lg mb-2 flex items-center justify-center">
@@ -64,9 +64,23 @@ const Menu = ({ isMenuOpen, setIsMenuOpen, addToCart, cart }) => {
               <p className="text-base font-bold mt-2">R$ {product.price.toFixed(2)}</p>
               <button
                 onClick={() => addToCart(product)}
-                className="mt-2 w-full bg-[#e63946] text-white py-2 px-4 rounded-full hover:bg-red-700 transition text-sm"
+                className="mt-2 w-full bg-[#e63946] text-white py-2 px-4 rounded-full hover:bg-red-700 transition text-sm flex items-center justify-center"
               >
-                Adicionar à Sacola
+                <svg
+                  className="h-5 w-5 mr-2"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                Adicionar ao Carrinho
               </button>
             </div>
           ))
