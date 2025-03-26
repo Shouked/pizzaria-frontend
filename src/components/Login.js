32a06c03eee6939f04a265abb87d11fdcf310b46
Shouked <136965203+Shouked@users.noreply.github.com>
2025-03-25 22:04:07 -0300

    Atualizar o Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
 import api from '../services/api';
 import { toast } from 'react-toastify';
 
-const Login = ({ setIsLoginOpen, setIsLoggedIn, setUser, navigate }) => {
-  const { tenantId } = useParams(); // Captura o tenantId da URL
+const Login = ({ tenantId, setIsLoginOpen, setIsLoggedIn, setUser, navigate, setIsRegisterOpen }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -38,7 +36,8 @@ const Login = ({ setIsLoginOpen, setIsLoggedIn, setUser, navigate }) => {
 
   return (
     <div>
-      <h2 className="text-lg font-semibold mb-4">Entrar</h2>
+      <h2 className="text-xl font-semibold text-center mb-4 text-[#e63946]">Entrar</h2>
+
       <input
         type="email"
         placeholder="Email"
@@ -46,6 +45,7 @@ const Login = ({ setIsLoginOpen, setIsLoggedIn, setUser, navigate }) => {
         onChange={(e) => setEmail(e.target.value)}
         className="w-full border border-gray-300 p-2 rounded mb-2"
       />
+
       <input
         type="password"
         placeholder="Senha"
@@ -53,12 +53,24 @@ const Login = ({ setIsLoginOpen, setIsLoggedIn, setUser, navigate }) => {
         onChange={(e) => setPassword(e.target.value)}
         className="w-full border border-gray-300 p-2 rounded mb-4"
       />
+
       <button
         onClick={handleLogin}
         className="bg-[#e63946] text-white px-4 py-2 rounded w-full hover:bg-red-600"
       >
         Entrar
       </button>
+
+      <button
+        onClick={() => {
+          setIsLoginOpen(false);
+          setIsRegisterOpen(true);
+        }}
+        className="text-sm text-[#e63946] hover:underline w-full text-center mt-4"
+      >
+        Criar conta
+      </button>
+
       <button
         onClick={() => setIsLoginOpen(false)}
         className="bg-gray-300 text-black px-4 py-2 rounded w-full mt-2 hover:bg-gray-400"
